445b25f4dbe16cadca239c1df4d0521585866d58
Diksha Rai <143122772+DikshaRai0761@users.noreply.github.com>
2026-07-25 13:09:24 +0530

    Time: 4 ms (100%), Space: 73.2 MB (55.41%) - LeetHub

---
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int minKBitFlips(int[] nums, int k) {
+
+        int n = nums.length;
+
+        int[] isFlipped = new int[n];
+
+        int flip = 0;
+
+        int ans = 0;
+
+        for(int i=0;i<n;i++){
+
+            if(i>=k){
+                flip -= isFlipped[i-k];
+            }
+
+            if(nums[i] == (flip % 2)){
+
+                if(i+k>n)
+                    return -1;
+
+                ans++;
+
+                flip++;
+
+                isFlipped[i]=1;
+            }
+        }
+
+        return ans;
+    }
+}
